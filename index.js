12b9bcb380b9078c4dd0d96bb59ce6b6103324d5
Shivam Sharma <75516122+emshivam01@users.noreply.github.com>
2023-03-14 22:37:41 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-let nameArray = ["Mike", "Ross", "Joe", "George", "John"];
-
-for (let i = 0; i < nameArray.length; i++) {
-  if (nameArray[i][0] == "J" || nameArray[i][0] == "j") {
-    console.log("Goodbye " + nameArray[i]);
-  } else {
-    console.log("Hello " + nameArray[i]);
-  }
-}
